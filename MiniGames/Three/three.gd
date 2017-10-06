5eb76bd7c47c266945ec2124044d8d7d757e0bde
Craig Smith <agent.craig@gmail.com>
2017-10-06 11:21:34 -0700

    Initial functioning minigame with placeholders

--- MiniGames/Three/three.gd
+++ MiniGames/Three/three.gd
@@ -5,12 +5,34 @@ export(int) var number_of_clothes = 6
 onready var timer = get_node("Timer")
 onready var item = get_node("Clothes/Item")
 var last_time = 0
+var score = 0
 
 func game_failed():
-	pass
+	print("FAILED")
 
 func game_passed():
-	pass
+	print("SUCCESS")
+
+func item_selected(selected):
+	if not selected.is_good_item():
+		game_failed()
+	selected.hide()
+	score += 1
+	if score > 2:
+		game_passed()
+
+func add_item(sprite, good):
+	var new_item = item.duplicate(true)
+	get_node("Clothes").add_child(new_item)
+	new_item.set_normal_texture(load(sprite))
+	new_item.set_good_item(good)
+	new_item.connect("pressed", self, "item_selected", [new_item])
+
+func randomize_items():
+	for child in get_node("Clothes").get_children():
+		if child.is_visible():
+			# TODO: Ensure no overlap
+			child.set_pos(Vector2(randi() % 600 + 10, randi() % 550 + 40))
 
 func update_time(time_left):
 	# Optional could make time bounce or move for each second changed
@@ -28,6 +50,13 @@ func _process(delta):
 		update_time(time_left)
 
 func _ready():
+	add_item("res://RawAssets/Sketches/pant_ph.png", true)
+	add_item("res://RawAssets/Sketches/shirt_ph.png", true)
+	add_item("res://RawAssets/Sketches/tie_ph.png", true)
+	add_item("res://RawAssets/Sketches/beenie_ph.png", false)
+	add_item("res://RawAssets/Sketches/raygun_ph.png", false)
+	add_item("res://RawAssets/Sketches/clown_shoes_ph.png", false)
+	randomize_items()
 	timer.set_wait_time(time_limit)
 	timer.start()
 	set_process(true)
