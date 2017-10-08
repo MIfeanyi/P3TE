cf1538016f8f4c74f398ebbeae692135aca5cc79
Michael Redford <michaeliredford@gmail.com>
2017-10-08 12:17:22 -0600

    thumbs up script

---
+++ MiniGames/One/Thumbs up.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+export var velocity = Vector2()
+
+func _ready():
+	set_process(true)
+	pass
+
+func _process(delta):
+	translate(velocity * delta)
+	if(get_pos().y >= get_viewport_rect().size.height):
+		queue_free()
+		pass 
+	pass
